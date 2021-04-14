68308f74208c8aee951e3c2b3a2f824ea95c4e3c
= <=>
2021-04-14 16:35:43 +0800

    add validate

--- src/components/Warranty.vue
+++ src/components/Warranty.vue
@@ -36,15 +36,15 @@
               <Field v-model="form.county" class="form-control w-50" id="county" name="county"
                :class="{ 'is-invalid': errors['county'] }" rules="required" data-role="county" data-name="county" data-style="form-control"
                 as="select"></Field>
-              <error-message name="county" class="invalid-feedback"></error-message>
+              <!-- <error-message name="county" class="invalid-feedback"></error-message> -->
               <Field v-model="form.district" class="form-control w-25" id="district[]" name="district[]"
                :class="{ 'is-invalid': errors['district[]'] }" rules="required" data-role="district" data-name="district[]" data-style="form-control"
                as="select"></Field>
-              <error-message name="district[]" class="invalid-feedback"></error-message>
+              <!-- <error-message name="district[]" class="invalid-feedback"></error-message> -->
               <Field v-model="form.zipcode" class="form-control w-25" id="zipcode" name="zipcode"
                :class="{ 'is-invalid': errors['zipcode'] }" rules="required" data-role="zipcode" data-name="zipcode" data-style="form-control"
                as="input"></Field>
-              <error-message name="zipcode" class="invalid-feedback"></error-message>
+              <!-- <error-message name="zipcode" class="invalid-feedback"></error-message> -->
               <!-- 原本的 jQuery 的 "郵遞區號" 程式碼 -->
               <!-- <div data-role="county" data-name="county"
                 data-style="form-control" class="w-50">
@@ -57,14 +57,25 @@
               </div> -->
             </div>
           </div>
+          <p>你選的是 {{ form.county }} 、 {{ form.district }} </p>
+          <p>( 為什麼 " 縣市區域 " 一定要 change 才會帶入 data 裡 )</p>
+          <p class="text-danger">( 畫面的欄位內容 " 縣市區域 " 和 data 資料比對，若無 change 就會不符 )</p>
           <div class="form-group text-left">
             <Field v-model="form.address" class="form-control" type="text" id="地址" name="地址"
                    :class="{ 'is-invalid': errors['地址'] }" rules="required"></Field>
             <error-message name="地址" class="invalid-feedback"></error-message>
           </div>
+          <div class="form-group text-left">
+            <Field v-model="sex" class="form-control" type="text" id="sex" name="sex"
+                :class="{ 'is-invalid': errors['sex'] }" rules="required" as="select">
+              <option value="">請選填 SEX</option>
+              <option value="boy">boy</option>
+              <option value="girl">girl</option>
+            </Field>
+            <error-message name="sex" class="invalid-feedback"></error-message>
+          </div>
         </div>
       </div>
-      <div class="d-flex"></div>
       <button class="btn btn-primary btn-block" type="submit" style="margin-top: 15px;">送出</button>
     </Form>
   </div>
@@ -80,6 +91,7 @@ export default {
       手機: 'required|min:10'
     }
     return {
+      sex: '',
       schema,
       form: {
         code: '',
@@ -100,13 +112,19 @@ export default {
   mounted () {
     $('#twzipcode').twzipcode()
   },
-  methods: {
-    store () {
+  created () {
     const vm = this
     vm.form.county = $('#twzipcode').twzipcode('get', 'county')[0]
     vm.form.district = $('#twzipcode').twzipcode('get', 'district')[0]
-    vm.form.zipcode = $('#twzipcode').twzipcode('get', 'zipcode')[0]
-    },
+    vm.form.zipcode = $('#twzipcode').twzipcode('get', 'zipcode')[0]  
+  },
+  methods: {
+    // store () {
+    //   const vm = this
+    //   vm.form.county = $('#twzipcode').twzipcode('get', 'county')[0]
+    //   vm.form.district = $('#twzipcode').twzipcode('get', 'district')[0]
+    //   vm.form.zipcode = $('#twzipcode').twzipcode('get', 'zipcode')[0]
+    // },
     isPhone (value) {
       const phoneNumber = /^(09)[0-9]{8}$/
       return phoneNumber.test(value) ? true : '需要正確的電話號碼'
